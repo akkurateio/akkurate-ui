15695dd13dd997bc289f3963ac6d06b0b834f9e2
vincent crosnier <92791698+vincentpresentation12@users.noreply.github.com>
2022-08-22 12:43:30 +0200

    icon

--- packages/icons/src/SpinnerAlternative.tsx
+++ packages/icons/src/SpinnerAlternative.tsx
@@ -1,1 +1,20 @@
-<?xml version="1.0" encoding="UTF-8"?><svg id="Calque_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><defs><style>.cls-1{opacity:.1;}.cls-2{opacity:.4;}.cls-3{opacity:.35;}.cls-4{opacity:.55;}.cls-5{opacity:.85;}.cls-6{opacity:.7;}</style></defs><circle cx="16" cy="5" r="2"/><circle class="cls-2" cx="16" cy="27" r="2"/><circle class="cls-4" cx="8.222" cy="23.778" r="2"/><circle class="cls-1" cx="27" cy="16" r="2"/><circle class="cls-6" cx="5" cy="16" r="2"/><circle class="cls-3" cx="23.778" cy="23.778" r="2"/><circle class="cls-5" cx="8.222" cy="8.222" r="2"/></svg>
+import { createIcon } from "@chakra-ui/icon"
+
+export const SpinnerAlternativeIcon = createIcon({
+    displayName: "SpinnerAlternativeIcon",
+    viewBox: "0 0 32 32",
+    path: (
+        <g fill="currentColor">
+            <defs>
+                <style>.cls-1{"opacity:.1;"}.cls-2{"opacity:.4;"}.cls-3{"opacity:.35;"}.cls-4{"opacity:.55;"}.cls-5{"opacity:.85;"}.cls-6{"opacity:.7;"}</style>
+            </defs>
+            <circle cx="16" cy="5" r="2"/>
+            <circle className="cls-2" cx="16" cy="27" r="2"/>
+            <circle className="cls-4" cx="8.222" cy="23.778" r="2"/>
+            <circle className="cls-1" cx="27" cy="16" r="2"/>
+            <circle className="cls-6" cx="5" cy="16" r="2"/>
+            <circle className="cls-3" cx="23.778" cy="23.778" r="2"/>
+            <circle className="cls-5" cx="8.222" cy="8.222" r="2"/>
+        </g>
+    )
+})
