290d6e8315c815edcd116be5a025ef1ab4b67b23
Benjamin <benjamin.francois13@gmail.com>
2022-08-22 13:35:16 +0200

    Fix prettier

--- packages/icons/src/Eye.tsx
+++ packages/icons/src/Eye.tsx
@@ -1,5 +1,4 @@
-import { createIcon } from "@chakra-ui/react";
-
+import { createIcon } from "@chakra-ui/react"
 
 export const Eye = createIcon({
   displayName: "Eye",
--- packages/icons/src/Underline.tsx
+++ packages/icons/src/Underline.tsx
@@ -1,11 +1,12 @@
 import { createIcon } from "@chakra-ui/icon"
 
 export const UnderlineIcon = createIcon({
-    displayName: "UnderlineIcon",
-    viewBox: "0 0 32 32",
-    path: (
-        <g fill="currentColor">
-            <path d="M16,23c3.86,0,7-3.141,7-7V6c0-.553-.448-1-1-1s-1,.447-1,1v10c0,2.757-2.243,5-5,5s-5-2.243-5-5V6c0-.553-.448-1-1-1s-1,.447-1,1v10c0,3.859,3.14,7,7,7Z"/><path d="M27,25H5c-.552,0-1,.447-1,1s.448,1,1,1H27c.552,0,1-.447,1-1s-.448-1-1-1Z"/>
-        </g>
-    )
+  displayName: "UnderlineIcon",
+  viewBox: "0 0 32 32",
+  path: (
+    <g fill="currentColor">
+      <path d="M16,23c3.86,0,7-3.141,7-7V6c0-.553-.448-1-1-1s-1,.447-1,1v10c0,2.757-2.243,5-5,5s-5-2.243-5-5V6c0-.553-.448-1-1-1s-1,.447-1,1v10c0,3.859,3.14,7,7,7Z" />
+      <path d="M27,25H5c-.552,0-1,.447-1,1s.448,1,1,1H27c.552,0,1-.447,1-1s-.448-1-1-1Z" />
+    </g>
+  ),
 })
