08073261b0dc8edafc97aa42d1891e14aac30aca
vincent crosnier <92791698+vincentpresentation12@users.noreply.github.com>
2022-09-13 17:05:53 +0200

    value obligatoire pour textarea

---
+++ packages/forms/src/InputPhone.tsx
@@ -0,0 +1,189 @@
+import React, { useState } from "react"
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormControlOptions,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  HTMLChakraProps,
+  Image,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Popover,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react"
+import { AisChevronDown } from "@akkurateio/icons"
+import { ThemingProps } from "@chakra-ui/system"
+import { FunctionComponent } from "react"
+import FormControlLayout from "./FormControlLayout"
+
+type Omitted = "disabled" | "required" | "readOnly" | "size" | "value"
+
+interface InputOptions {
+  value?: string
+  handleChange: (e: string) => void
+  focusBorderColor?: string
+  errorBorderColor?: string
+  htmlSize?: number
+  label?: string
+  error?: string
+  hint?: string
+  countriesArray?: any[]
+}
+
+interface InputProps
+  extends Omit<HTMLChakraProps<"input">, Omitted>,
+    InputOptions,
+    ThemingProps<"Input">,
+    FormControlOptions {}
+
+export const AcsInputPhone: FunctionComponent<InputProps> = ({
+  handleChange,
+  ...props
+}) => {
+  const propsForInput = () => {
+    const {
+      label,
+      hint,
+      error,
+      isRequired,
+      isDisabled,
+      isInvalid,
+      isReadOnly,
+      size,
+      ...rest
+    } = props
+    return rest
+  }
+
+  const [phoneValue, setPhoneValue] = useState("")
+  const [valid, setValid] = useState("")
+  const [country, setCountry] = useState<{
+    name: string
+    icon: JSX.Element
+    code: string
+    prefix: string
+  }>(countriesArray[0])
+
+  const handleCountryChange = (
+    currentCountry: {
+      name: string
+      icon: JSX.Element
+      code: string
+      prefix: string
+    },
+    onClose: any,
+  ) => {
+    setCountry(currentCountry)
+    onClose()
+  }
+
+  const setPhoneNumber = (phoneNumb: any) => {
+    setPhoneValue(phoneNumb)
+    if (isNaN(phoneNumb)) {
+      throw new Error("Not a number")
+    } else {
+      setValid("success")
+      const slicedPhoneValue =
+        phoneNumb.slice(0, 3) +
+        " " +
+        phoneNumb.slice(3, 4) +
+        " " +
+        phoneNumb.slice(4, 6) +
+        " " +
+        phoneNumb.slice(6, 8) +
+        " " +
+        phoneNumb.slice(8, 10) +
+        " " +
+        phoneNumb.slice(10, 12)
+      return slicedPhoneValue
+    }
+  }
+
+  return (
+    <FormControl
+      paddingRight={5}
+      paddingLeft={40}
+      isDisabled={false}
+      isInvalid={false}
+      isRequired={true}
+      variant="filled"
+    >
+      <FormLabel>Telephone</FormLabel>
+      <InputGroup>
+        <InputLeftElement
+          width={"90px"}
+          display={"flex"}
+          justifyContent={"space-between"}
+        >
+          <Popover>
+            {({ isOpen, onClose }) => (
+              <>
+                <PopoverTrigger>
+                  <Flex alignItems={"center"}>
+                    <Button
+                      variant={"unstyled"}
+                      color={"red.500"}
+                      _hover={{ color: "red.700" }}
+                      _active={{ color: "red.700" }}
+                      marginLeft={2}
+                      marginRight={-3}
+                    >
+                      {country.icon}
+                    </Button>
+                    <AisChevronDown />
+                  </Flex>
+                </PopoverTrigger>
+                <PopoverContent overflow={"hidden"} w={20} h={72}>
+                  <PopoverBody overflow={"hidden"}>
+                    <SimpleGrid columns={1} h={"full"} overflow={"hidden"}>
+                      <Box h={"full"} overflowY={"auto"}>
+                        <SimpleGrid spacingY={0.5}>
+                          {countriesArray.map((country, index) => (
+                            <Button
+                              key={index}
+                              onClick={() =>
+                                handleCountryChange(country, onClose)
+                              }
+                              size={"sm"}
+                            >
+                              {country.icon}
+                            </Button>
+                          ))}
+                        </SimpleGrid>
+                      </Box>
+                    </SimpleGrid>
+                  </PopoverBody>
+                </PopoverContent>
+              </>
+            )}
+          </Popover>
+          <Text minWidth={"40px"} maxWidth={"40px"} paddingLeft={"10px"}>
+            {country.prefix}
+          </Text>
+        </InputLeftElement>
+        <Input
+          {...propsForInput()}
+          variant={props.variant}
+          placeholder={"Numéro de téléphone"}
+          width={"auto"}
+          paddingLeft={"95px"}
+          maxLength={9}
+          onChange={(phoneNumb) =>
+            setPhoneNumber(country.prefix + phoneNumb.target.value)
+          }
+        />
+      </InputGroup>
+      <FormHelperText>Need help ? </FormHelperText>
+      <FormErrorMessage>rentrer du texte</FormErrorMessage>
+    </FormControl>
+  )
+}
