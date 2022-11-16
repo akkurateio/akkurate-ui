17c5ef80100f5299859e1eabe933410e1c3d8b41
vincent crosnier <92791698+vincentpresentation12@users.noreply.github.com>
2022-11-16 12:46:14 +0100

    tree structure first version

--- packages/components/src/advanced/TreeStructure/Tree.tsx
+++ packages/components/src/advanced/TreeStructure/Tree.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import {InterfaceTree} from "./InterfaceTree";
-import TreeSecond from "./TreeSecond";
-import FormControlLayout from "@akkurateio/forms/src/FormControlLayout";
-
+import React from "react"
+import { InterfaceTree } from "./InterfaceTree"
+import TreeSecond from "./TreeSecond"
+import FormControlLayout from "@akkurateio/forms/src/FormControlLayout"
 
 interface TreeOptions {
-    tree: InterfaceTree[];
+  tree: InterfaceTree[]
 }
 
-export const Tree: React.FC<TreeOptions> = ({tree, ...props}) => {
-    return (
-        <FormControlLayout {...props}>
-            <TreeSecond tree={tree} />
-        </FormControlLayout>
-    );
-};
+export const AcsTree: React.FC<TreeOptions> = ({ tree, ...props }) => {
+  return (
+    <FormControlLayout {...props}>
+      <TreeSecond tree={tree} />
+    </FormControlLayout>
+  )
+}
 
-export default Tree;
+export default AcsTree
