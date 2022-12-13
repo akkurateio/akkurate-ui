4c4cba113772875b54d8051a091915c6dd57f67b
vincent crosnier <92791698+vincentpresentation12@users.noreply.github.com>
2022-12-13 11:09:28 +0100

    input phone design

--- packages/forms/src/InputPhone.tsx
+++ packages/forms/src/InputPhone.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import {
   Box,
   Button,
   FormControlOptions,
   FormLabel,
+  HStack,
   HTMLChakraProps,
   InputGroup,
   InputLeftAddon,
@@ -14,6 +15,8 @@ import {
   SimpleGrid,
   Tooltip,
 } from "@chakra-ui/react"
+// @ts-ignore
+import { theme } from "@akkurateio/utils"
 // @ts-ignore
 import isoCountriesLanguages from "iso-countries-languages"
 import { countries, Country } from "countries-list"
@@ -56,6 +59,7 @@ export const AcsInputPhone: React.FC<InputProps> = ({
 }) => {
   const [isOpen, setIsOpen] = useState(false)
   const [country, setCountry] = useState<string>(defaultCountry)
+  const [focus, setFocus] = useState(false)
 
   const changeCountry = (country: string) => {
     setCountry(country)
@@ -75,10 +79,33 @@ export const AcsInputPhone: React.FC<InputProps> = ({
     }
   })
 
+  const countryLower = country.toLowerCase()
+
   return (
     <FormControlLayout {...props}>
-      <InputGroup size={props.size}>
-        <InputLeftAddon backgroundColor={"white"}>
+      <InputGroup
+        size={props.size}
+        width={"fit-content"}
+        rounded={"4px"}
+        onFocus={() => setFocus(true)}
+        onBlur={() => setFocus(false)}
+        borderWidth={"1px"}
+        borderColor={focus ? "primary.500" : "neutral.400"}
+        boxShadow={
+          focus ? `0 0 0 3px ${theme.colors.primary[500]}25 ` : undefined
+        }
+      >
+        <InputLeftAddon
+          outline={"none"}
+          rounded={"4px"}
+          backgroundColor={"white"}
+          border={"none"}
+          width={"83px"}
+          paddingLeft={"10px"}
+          paddingTop={"6px"}
+          paddingBottom={"6px"}
+          paddingRight={"7px"}
+        >
           <Popover>
             <PopoverTrigger>
               <Button
@@ -86,29 +113,45 @@ export const AcsInputPhone: React.FC<InputProps> = ({
                 onChange={() => setIsOpen(true)}
                 display={"flex"}
                 alignItems={"center"}
+                border={"none"}
               >
-                <Flag code={country} width={"32px"} />
-                <AisChevronDown marginLeft={2} />
+                <HStack spacing={"16px"}>
+                  <Flag code={country} width={"32px"} />
+                  <AisChevronDown />
+                </HStack>
               </Button>
             </PopoverTrigger>
-            <PopoverContent
-              overflow={"hidden"}
-              width={"90px"}
-              h={72}
-              backgroundColor={"blue.50"}
-            >
-              <PopoverBody overflow={"hidden"}>
-                <Box pr={3} h={"full"} overflowY={"auto"}>
-                  <SimpleGrid columns={1}>
+            <PopoverContent overflow={"hidden"} width={"85px"} h={"226px"}>
+              <PopoverBody
+                overflow={"hidden"}
+                overflowY={"auto"}
+                padding={0}
+                paddingLeft={"5px"}
+              >
+                <Box h={"full"}>
+                  <SimpleGrid columns={1} spacing={"2px"} paddingTop={"5px"}>
                     {frenchCountryWithCode.map((country, idx) => {
                       return (
                         <Tooltip key={idx} label={country.name}>
                           <Button
-                            width={15}
+                            width={"62px"}
+                            height={"34px"}
                             onClick={() => changeCountry(country.code)}
                             variant={"unstyled"}
+                            backgroundColor={
+                              country.code.toLowerCase() === countryLower
+                                ? "primary.500"
+                                : "primary.100"
+                            }
+                            rounded={"2px"}
+                            justifyContent={"center"}
+                            display={"flex"}
                           >
-                            <Flag code={country.code} />
+                            <Flag
+                              width={"32px"}
+                              height={"24px"}
+                              code={country.code}
+                            />
                           </Button>
                         </Tooltip>
                       )
@@ -121,17 +164,27 @@ export const AcsInputPhone: React.FC<InputProps> = ({
         </InputLeftAddon>
         <PhoneInput
           specialLabel={""}
-          country={country.toLowerCase()}
+          country={countryLower}
           onChange={(phoneNumber) => handleChange("+" + phoneNumber)}
           countryCodeEditable={false}
           value={props.value}
           inputStyle={{
             width: "100%",
             height: "100%",
-            paddingLeft: "10px",
-            border: "1px solid #E2E8F0",
-            borderTopRightRadius: "5px",
-            borderBottomRightRadius: "5px",
+            paddingLeft: "12px",
+            paddingTop: "10.5px",
+            paddingBottom: "10.5px",
+            borderLeft: props.isInvalid
+              ? `1px solid ${theme.colors.red[600]}`
+              : focus
+              ? `1px solid ${theme.colors.primary[500]}`
+              : `1px solid ${theme.colors.neutral[400]}`,
+            borderTopRightRadius: "4px",
+            borderBottomRightRadius: "4px",
+            outline: "none",
+            fontSize: "14px",
+            fontStyle: "inter",
+            fontWeight: "regular",
           }}
         />
       </InputGroup>
