0335dede687dc4fca69b47337451c6c64f48c4e2
Olivier Lacombe <369876+olacombe@users.noreply.github.com>
2022-12-14 15:54:26 +0100

    Ajout de nouvels icons

---
+++ packages/icons/src/icons/AisFileXml.tsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { createIcon } from "@chakra-ui/icon"
+
+export const AisFileXml = createIcon({
+  displayName: "AisFileXml",
+  viewBox: "0 0 32 32",
+  
+  defaultProps: {
+		boxSize: "24px",
+		fill: "currentColor",
+	},
+  path: (
+    <>
+      <path d="M17.196 12.02a1 1 0 0 1 .785 1.176l-2 10a1 1 0 0 1-1.962-.392l2-10a1 1 0 0 1 1.177-.785ZM12.832 15.555a1 1 0 0 0-1.664-1.11l-2 3a1 1 0 0 0 0 1.11l2 3a1 1 0 0 0 1.664-1.11L11.202 18l1.63-2.445ZM19.168 20.445a1 1 0 0 0 1.664 1.11l2-3a1 1 0 0 0 0-1.11l-2-3a1 1 0 0 0-1.664 1.11L20.798 18l-1.63 2.445Z" /><path fillRule="evenodd" clipRule="evenodd" d="M8 2h12.414L26 7.586V28c0 1.103-.897 2-2 2H8c-1.103 0-2-.897-2-2V4c0-1.103.897-2 2-2Zm15.586 6L20 4.414V8h3.586ZM8 4v24h16V10h-6V4H8Z" />
+    </>
+  ),
+})
