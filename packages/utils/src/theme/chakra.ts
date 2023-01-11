38e8fd33518b0c8a99ea695987322cc0cbba25f8
vincent crosnier <92791698+vincentpresentation12@users.noreply.github.com>
2023-01-11 16:56:15 +0100

    just because Someone want different props name and because rounded for search

--- packages/utils/src/theme/chakra.ts
+++ packages/utils/src/theme/chakra.ts
@@ -229,6 +229,7 @@ const styles = {
       ...markdownCss,
     },
     ".ProseMirror": {
+      border: "none",
       width: "100%",
       height: "100%",
       minHeight: "100px",
@@ -239,6 +240,8 @@ const styles = {
     },
     ".ProseMirror-focused": {
       outlineColor: "primary.500",
+      outline: "none",
+      border: "none",
     },
   }),
 }
