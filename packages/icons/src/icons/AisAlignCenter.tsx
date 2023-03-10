2de01083c42565bfff47e5d206e71661d87fbce7
vincent crosnier <92791698+vincentpresentation12@users.noreply.github.com>
2023-03-10 13:50:44 +0100

    icons akkurate

---
+++ packages/icons/src/icons/AisAlignCenter.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import { createIcon } from "@chakra-ui/icon"
+
+export const AisAlignCenter = createIcon({
+  displayName: "AisAlignCenter",
+  viewBox: "0 0 32 32",
+  defaultProps: {
+		fill: "currentColor",
+    boxSize: "24px",
+	},
+  path: (
+    <>
+      <path fillRule="evenodd" clipRule="evenodd" d="M4 5a1 1 0 0 0 0 2h24a1 1 0 1 0 0-2H4Zm3 4a1 1 0 0 0 0 2h18a1 1 0 1 0 0-2H7Zm1 5a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H9a1 1 0 0 1-1-1Zm-3 3a1 1 0 1 0 0 2h22a1 1 0 1 0 0-2H5Zm-2 5a1 1 0 0 1 1-1h24a1 1 0 1 1 0 2H4a1 1 0 0 1-1-1Zm8 3a1 1 0 1 0 0 2h10a1 1 0 1 0 0-2H11Z" />
+    </>
+  ),
+})
---
+++ packages/icons/src/icons/AisAlignLeft.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import { createIcon } from "@chakra-ui/icon"
+
+export const AisAlignLeft = createIcon({
+  displayName: "AisAlignLeft",
+  viewBox: "0 0 32 32",
+  defaultProps: {
+		fill: "currentColor",
+    boxSize: "24px",
+	},
+  path: (
+    <>
+      <path fillRule="evenodd" clipRule="evenodd" d="M4 5a1 1 0 0 0 0 2h24a1 1 0 1 0 0-2H4Zm0 4a1 1 0 0 0 0 2h18a1 1 0 1 0 0-2H4Zm-1 5a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H4a1 1 0 0 1-1-1Zm1 3a1 1 0 1 0 0 2h22a1 1 0 1 0 0-2H4Zm-1 5a1 1 0 0 1 1-1h24a1 1 0 1 1 0 2H4a1 1 0 0 1-1-1Zm1 3a1 1 0 1 0 0 2h10a1 1 0 1 0 0-2H4Z" />
+    </>
+  ),
+})
